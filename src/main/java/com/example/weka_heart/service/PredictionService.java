fbc05d97aa8eef7854b7223a78894f65c5afd2ba
JESUS <ribonjesus123@gmail.com>
2025-05-26 21:01:59 -0500

    Back listo

--- src/main/java/com/example/weka_heart/service/PredictionService.java
+++ src/main/java/com/example/weka_heart/service/PredictionService.java
@@ -29,7 +29,7 @@ public class PredictionService {
     private List<PatientPrediction> predictionResults = new ArrayList<>();
     private int currentId = 1;
 
-    private String apiKey = "Bearer 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";
+    private String apiKey = "Bearer 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";
 
     public PredictionService() {
         try {
